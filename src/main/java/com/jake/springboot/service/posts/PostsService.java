c3e7a15606d35c1eb2817bae744bd12e8bf98cc3
Jaeseok Yoon <yjaeseok@gmail.com>
2020-05-03 22:11:48 +0900

    Implement PostsService and Dto and Controller

---
+++ src/main/java/com/jake/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.jake.springboot.service.posts;
+
+import com.jake.springboot.domain.posts.PostsRepository;
+import com.jake.springboot.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
